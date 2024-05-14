9bc02582df5ad1a5bcd308eca37ef840d2170be8
51LV3Rok <56646530+ssnagin@users.noreply.github.com>
2024-05-14 20:43:06 +0300

    something

---
+++ distribution_build_config.cmake
@@ -0,0 +1,9 @@
+# Installed build configuration
+set(USE_QT 5)
+set(KUMIR2_ROOT P:/Program Files (x86)/Kumir2)
+set(KUMIR2_SDK_SCRIPTS_DIR include)
+set(KUMIR2_SDK_SRC_DIR src)
+set(GIT_HASH )
+set(GIT_TAG )
+set(GIT_BRANCH )
+set(GIT_TIMESTAMP )
---
+++ make_windows.bat
@@ -0,0 +1,2 @@
+cmake -DCMAKE_BUILD_TYPE=Release -G "NMake Makefiles" ..
+cmake -G "MinGW Makefiles"
